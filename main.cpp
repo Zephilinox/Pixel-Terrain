0b0f455b8a0f8ab9a497f99b0c1b4d9725fcd9e9
Zephilinox <Zephilinox@hotmail.co.uk>
2014-10-17 19:08:02 +0100

    Created PixelShape class

--- main.cpp
+++ main.cpp
@@ -10,6 +10,48 @@
 
 //SELF
 
+class PixelShape : public sf::Drawable
+{
+public:
+
+    PixelShape()
+    {
+        m_image.create(100, 100, sf::Color::Red);
+        m_texture.loadFromImage(m_image);
+        m_sprite.setTexture(m_texture);
+        m_sprite.setPosition(100, 100);
+    }
+
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+    {
+        target.draw(m_sprite, states);
+    }
+
+    void destroy(sf::Vector2i pos, int radius)
+    {
+        for (int x = pos.x - radius/2; x < pos.x + radius/2; ++x)
+        {
+            for (int y = pos.y - radius/2; y < pos.y + radius/2; ++y)
+            {
+                if (x >= m_sprite.getPosition().x && x < m_sprite.getPosition().x + m_image.getSize().x &&
+                    y >= m_sprite.getPosition().y && y < m_sprite.getPosition().y + m_image.getSize().y)
+                {
+                    sf::Color p = m_image.getPixel(x - m_sprite.getPosition().x, y - m_sprite.getPosition().y);
+                    p.a = 0;
+                    m_image.setPixel(x - m_sprite.getPosition().x, y - m_sprite.getPosition().y, p);
+                    m_texture.loadFromImage(m_image);
+                    m_sprite.setTexture(m_texture, true);
+                }
+            }
+        }
+    }
+
+private:
+    sf::Image m_image;
+    sf::Texture m_texture;
+    sf::Sprite m_sprite;
+};
+
 int main()
 {
     std::srand(std::time(0));
@@ -17,8 +59,7 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 800, 32), "Pixel Terrain");
     window.setVerticalSyncEnabled(true);
 
-    sf::Image terrain;
-    terrain.create(100, 100, sf::Color::Red);
+    PixelShape terrain;
 
     sf::Event event;
     sf::Time prevFrameTime(sf::seconds(1.f / 60.f));
@@ -37,16 +78,7 @@ int main()
 
                 case sf::Event::MouseButtonPressed:
                 {
-                    for (unsigned int x = event.mouseButton.x - 10; x < event.mouseButton.x + 10; ++x)
-                    {
-                        for (unsigned int y = event.mouseButton.y - 10; y < event.mouseButton.y + 10; ++y)
-                        {
-                            sf::Color p = terrain.getPixel(x, y);
-                            p.a = 0;
-                            terrain.setPixel(x, y, p);
-                        }
-                    }
-
+                    terrain.destroy(sf::Vector2i(event.mouseButton.x, event.mouseButton.y), 10);
                 }
 
                 case sf::Event::KeyPressed:
@@ -63,10 +95,7 @@ int main()
 
         window.clear(sf::Color(40, 40, 40));
 
-        sf::Texture t;
-        t.loadFromImage(terrain);
-        sf::Sprite s(t);
-        window.draw(s);
+        window.draw(terrain);
 
         window.display();
 
