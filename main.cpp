97f655785d6c0af9282cb9ac6914c86a96f4308b
Zephilinox <Zephilinox@hotmail.co.uk>
2014-10-17 18:57:30 +0100

    Make block transparent

--- main.cpp
+++ main.cpp
@@ -17,6 +17,9 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 800, 32), "Pixel Terrain");
     window.setVerticalSyncEnabled(true);
 
+    sf::Image terrain;
+    terrain.create(100, 100, sf::Color::Red);
+
     sf::Event event;
     sf::Time prevFrameTime(sf::seconds(1.f / 60.f));
     sf::Clock frameClock;
@@ -34,6 +37,16 @@ int main()
 
                 case sf::Event::MouseButtonPressed:
                 {
+                    for (unsigned int x = event.mouseButton.x - 10; x < event.mouseButton.x + 10; ++x)
+                    {
+                        for (unsigned int y = event.mouseButton.y - 10; y < event.mouseButton.y + 10; ++y)
+                        {
+                            sf::Color p = terrain.getPixel(x, y);
+                            p.a = 0;
+                            terrain.setPixel(x, y, p);
+                        }
+                    }
+
                 }
 
                 case sf::Event::KeyPressed:
@@ -49,9 +62,15 @@ int main()
 
 
         window.clear(sf::Color(40, 40, 40));
+
+        sf::Texture t;
+        t.loadFromImage(terrain);
+        sf::Sprite s(t);
+        window.draw(s);
+
         window.display();
 
-        //std::cout << "FPS: " << 1.f / prevFrameTime.asSeconds() << "\n";
+        std::cout << "FPS: " << 1.f / prevFrameTime.asSeconds() << "\n";
         prevFrameTime = frameClock.restart();
     }
 
